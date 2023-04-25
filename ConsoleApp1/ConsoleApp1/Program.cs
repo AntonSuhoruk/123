5c3c49f33d7edc94a424fe42f83eb822c293d742
AntonSuhoruk <114994110+AntonSuhoruk@users.noreply.github.com>
2023-04-25 10:59:25 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib1 {
+    
+    public class calculation
+{
+    public double plus(double a, double b)
+    {
+        return a + b;
+    }
+
+    public double minus(double a, double b)
+    {
+        return a - b;
+    }
+
+    public double delit(double a, double b)
+    {
+        if (b == 0)
+        {
+            throw new DivideByZeroException("ошибка");
+        }
+        return a / b;
+    }
+    public double umnojit(double a, double b)
+    {
+        return a * b;
+    }
+}
+}
---
+++ ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MyLib1;
+
+namespace Proverka 
+{
+    class proverka1
+    {
+        static void Main(string[] args)
+        {
+            calculation calculator = new calculation();
+            Console.WriteLine("первое число");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("второе число");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("a = {0}, b={1}", a, b);
+            Console.WriteLine("a + b = {0}", calculator.plus(a, b));
+            Console.WriteLine("a - b = {0}", calculator.minus(a, b));
+            Console.WriteLine("a * b = {0}", calculator.umnojit(a, b));
+            Console.WriteLine("a / b = {0}", calculator.delit(a, b));
+        }
+    }
+}
